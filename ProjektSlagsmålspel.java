d48a366ed6efcb4e1e5346a48107bfbdf85a590d
Kevinvalsjo <123780539+Kevinvalsjo@users.noreply.github.com>
2023-02-05 22:31:05 +0100

    Update ProjektSlagsmålspel.java

--- ProjektSlagsmålspel.java
+++ ProjektSlagsmålspel.java
@@ -1,11 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package projektslagsmålspel;
+package javaapplication21;
 import java.util.Scanner;
-public class ProjektSlagsmålspel { 
-   
+public class JavaApplication21{ 
+        static String text="";
         public static void main(String[] args){  
        
         Scanner tangentbord = new Scanner(System.in);
@@ -18,14 +14,15 @@ public class ProjektSlagsmålspel {
         boolean play = true;
         System.out.println("Är du redo att starta? (ja/nej)");
         String svar = tangentbord.next();
-        
+        String lvl4 = "";
+        String lvl3 ="";
         if(svar.equalsIgnoreCase("ja")){
          int runda =1 ;
         int i = 6;
         int k = 3;
         int tal1  = (int) (Math.random()*k);
         System.out.println("Fienden börjar! Tryck (Enter) för att fortsätta");
-        
+        String fiende = "Johan, lvl 1";
         Scanner scan = new Scanner(System.in);
         scan.nextLine();
         
@@ -38,29 +35,50 @@ public class ProjektSlagsmålspel {
         { 
         if(poäng<20)
         {
-            while(fiendens_hp >=0 && ditt_hp >= 0){
-           
+            while(fiendens_hp >0 && ditt_hp > 0){
+               
+               if(poäng<4 && poäng >= 0)
+               {
+                System.out.println(fiende);
+               }
+               if(poäng>=4 && poäng<8)
+               {
+                   System.out.println(text);
+               
+                }
+                  if(poäng>=8 && poäng <12)
+                {
+                    System.out.println(lvl3);
+                }
+                   if(poäng>=12 && poäng <20)
+                   {
+                       System.out.println(lvl4);
+                   }
                 System.out.println("F_hp: "+fiendens_hp+  "\nD_hp: " + ditt_hp);
            
                 System.out.println("Din tur (Enter)");
                 int d_atk2  = (int) (Math.random()*i)+1;
+                fiendens_hp -= d_atk2;
                 int a = d_atk2;
                 scan.nextLine();
-                fiendens_hp -= d_atk2;
+                
                 System.out.println("Fienden förlorade "+d_atk2+" HP. ");
                 System.out.println("Fienden attackerar!! (Enter)");
                 scan.nextLine();
-                int c = tal1;
+               
                 int d_atk = (int) (Math.random()*k);
                 int b=d_atk;
                 
-                fält1[0]= a ;
+                fält1[0]= a;
                 fält1[1]=  b;
-                fält1[2]= c;
-                int differens1 = fält1[2]-fält1[0];
                 int differens2 = fält1[1]-fält1[0];
-                System.out.println("Differensen mellan era skador är:" + differens1);
-                System.out.println("Fiendens andra attack nedan minus din första:" + (differens2));
+              
+                //System.out.println("Fiendens andra attack nedan minus din första:" + (differens2));
+                if(fiendens_hp<=0 || ditt_hp<=0)
+                {
+                    fiendens_hp=0;
+                    break;
+                }
                 if(d_atk>0) 
                 {
                 System.out.println("Fienden fick en lyckad träff och gjorde "  + d_atk + " skada");
@@ -80,15 +98,16 @@ public class ProjektSlagsmålspel {
                 System.out.println("\nFienden parrerar 3 skada");
                 System.out.println("\nFienden har fortfarande "+ fiendens_hp +" HP"); 
                 }
+                
                 if(d_atk2 >25 || d_atk >25 )
                 {
                     int svärdskada = (int) (Math.random()*200);
-                    System.out.println("DU FICK SÅ MYCKET SKADA ATT DU KALLADE PÅ GUD OCH FICK ETT ONE TIME USE OCH GJORDE: " + svärdskada+"skada och fienden förlorade "+svärdskada+"hp");
+                    System.out.println("DU FICK SÅ MYCKET SKADA ATT DU KALLADE PÅ GUD OCH FICK ETT SVÄRD OCH GJORDE:" + svärdskada+" Skada och fienden förlorade "+svärdskada+" hp");
                     fiendens_hp -= svärdskada;
                     System.out.println(fiendens_hp);
                 }
-                    
-                if(d_atk2==i){
+                   System.out.println("Differensen mellan era skador är:" + differens2); 
+                if(d_atk2==i && d_atk2<=25){
                 System.out.println("\nDu fick maximerad skada och du öppnade nu ett vapenförråd. Vad vill du använda för vapen?, 1.Yxa , 2.Kniv 3.Knytnäve");
                 int val = tangentbord.nextInt();
                            switch(val){
@@ -100,14 +119,20 @@ public class ProjektSlagsmålspel {
                
                 if(yxa<25)
                 {
-                    System.out.println("Fienden hade en sopptunna där och blockade ditt slag och istället använde yxan till att öka sitt hp");
+                    System.out.println("Fienden hade en sköld där och blockade ditt slag och istället använde yxan till att öka sitt hp");
                     fiendens_hp += 10;
+                    
                       
                 }
                 else
                 {
+                    System.out.println("Du slog till med yxan och gjorde"+yxa +"skada");
                     fiendens_hp -=yxa;
-                      
+                    if(fiendens_hp<=0 || ditt_hp<=0)
+                {
+                    fiendens_hp=0;
+                    break;
+                }
                 }          
             }
                             case 2 -> {
@@ -124,16 +149,40 @@ public class ProjektSlagsmålspel {
                 int knivs =(int) Math.random()*20+10;
                 System.out.println("Du fick en lyckad träff och körde upp kniven rakt i örat och fienden förlorar " +knivs+ " hp");
                 fiendens_hp -= knivs;
+                 if(fiendens_hp<=0 || ditt_hp<=0)
+                {
+                    fiendens_hp=0;
+                    break;
+                }
                       
                 }        
             }
                             case 3 ->{
                 
                 int näve = (int) (Math.random()*20);
-                
+                if(näve>0)
+                {
                 System.out.println("Du slog till fienden hårt och gjorde " +näve+ " skada");
                 fiendens_hp -=näve;
                 System.out.println(fiendens_hp+ " " + ditt_hp);
+                 if(fiendens_hp<=0 || ditt_hp<=0)
+                {
+                    fiendens_hp=0;
+                    break;
+                }
+                
+                }
+                else
+                {
+                    System.out.println("Du missade fienden och blev uppercuttad och ramlade ner på marken och förloade 7 hp" );
+                    ditt_hp -= 7;
+                     if(fiendens_hp<=0 || ditt_hp<=0)
+                {
+                    ditt_hp=0;
+                    break;
+                }
+                     
+                }
                                      }
                        
                 }
@@ -142,12 +191,12 @@ public class ProjektSlagsmålspel {
         System.out.println("F_hp:"+fiendens_hp+ "\nD_hp"+ditt_hp);
         
         
-        
+        if(o==0){
         if(fiendens_hp<=0)
             
         {
             
-            System.out.println("Du vinner");
+            System.out.println("Du Mördade Fienden!");
             
         }
         
@@ -156,10 +205,15 @@ public class ProjektSlagsmålspel {
         {
             
             System.out.println("Du förlorade");
+           
+             ditt_hp=0;
+             System.out.println("Ditt hp är: "+ditt_hp+" ,noob");
+                
             
             
             break;
         } 
+        }
        
          runda ++;
         
@@ -175,62 +229,80 @@ public class ProjektSlagsmålspel {
                 k += 1;
                 ditt_hp = 200;
                 System.out.println("Fienden har nu blivit starkare. +1 skada & mer hp ");
-                poäng += Math.random()*4+1;
+                poäng += Math.random()*5+1;
                 System.out.println("poäng:"+poäng);
                 fiendens_hp = poäng*3 +fiendens_starthp;
-            }
-             else{
+                if(poäng>=4 && poäng<8)
+                {
+                  
+                    text = fiende.replace("Johan, lvl 1","Anders, lvl 2");
+                    k +=1;
+                    System.out.println("Anders har +1 mer skada");
+                }
+                if(poäng>=8 && poäng <12)
+                {
+                   
+                  lvl3= text.replace("Anders, lvl 2","Sopptunna, lvl 3");
+                   k +=2;
+                    System.out.println("Sopptunna har +2 mer skada");
+                }
+                if(poäng>=12 && poäng <20)
+                {
+                    
+                
+                     lvl4 = lvl3.replace("Sopptunna, lvl 3","Samurai, lvl 4");
+                     k +=4;
+                     System.out.println("Samurai har +4 mer skada");
+                }
+                
+             
+            } 
+            else
+            {
                 System.out.println("okej, programmet avslutas");
                 play = false;
-            }
-        } 
-        else
-        {
-            break;
-        }
-        
-            
-           
-           
+            }  
             
     }
-        else{
-            System.out.println("Du har fått 20 poäng och har kommit till sista bossen!");
-            poäng -=100;
-            k = 16;
-            fiendens_hp += 1000;
-            ditt_hp = 400;
-            i +=30;
-            o += 20;
-            
+        else
+           { 
+               if(fiendens_hp<=0){
+            System.out.println("DU VANN ÖVER BOSSEN! GRATTIS! Spelet avslutas");
+           play = false;
+               }
+               else
+               {
+                   System.out.println("Du förlorade över bossen:( hejdå");
+                   play = false;
+               }
+        }  
+        
+       
+                
             
          
             }
-        }
-       
+         else
+            {
+            System.out.println("Du har fått 20 eller mer poäng och har kommit till sista bossen!");
+            poäng -=100;
+            k = 16;
+            fiendens_hp += 3000;
+            ditt_hp = 600;
+            i +=30;
+            o += 20;
             
-        
-      
+            }    
     }
-        else{
+       
+        
+        }
+         else
+        {
              System.out.println("Vad synd att du inte ville spela!");
             }   
-        
-        }
    
             
      
 }
-
-
-     
-            
-
-    
-           
- 
-
-    
-
-
-
+}
